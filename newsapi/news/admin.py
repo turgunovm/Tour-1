0f24f94744913d5b83f4a23640be896a41dc6770
turgunovm <turgunovm@yahoo.com>
2019-09-02 12:13:29 +0500

    fixed admin view

--- newsapi/news/admin.py
+++ newsapi/news/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import  TourPack, Client, ClientTour
 
 
+class TourPackAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "price", "from_to", "start_date", "end_date", "quantity")
+
 # admin.site.register(Article)
-admin.site.register(TourPack)
+admin.site.register(TourPack, TourPackAdmin)
 admin.site.register(Client)
 admin.site.register(ClientTour)
